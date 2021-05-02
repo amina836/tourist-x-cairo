b8fa0f26a7fb152922f5d7a1e7bfc6e4aabf6a59
amina836 <ae645281@gmail.com>
2021-05-02 22:20:49 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// // religious places
+$(document).ready(function(){
+  $(".flip1").click(function(){
+    $(".panel1").slideToggle("slow");
+  });
+
+
+
+  $(".flip2").click(function(){
+    $(".panel2").slideToggle("slow");
+  });
+
+
+  $(".flip3").click(function(){
+    $(".panel3").slideToggle("slow");
+  });
+
+
+  $(".flip4").click(function(){
+    $(".panel4").slideToggle("slow");
+  });
+
+  $(".flip5").click(function(){
+    $(".panel5").slideToggle("slow");
+  });
+
+  $(".flip6").click(function(){
+    $(".panel6").slideToggle("slow");
+  });
+});
+
+// hotels
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+
+
+
+    $('[data-toggle="popover1"]').popover();   
+
+
+
+    $('[data-toggle="popover2"]').popover();   
+ });   
+
